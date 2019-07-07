fffa578da3d083b667b8b3185d27ed517972b0a9
aman-lohan-au2 <swishlohan420@gmail.com>
2019-07-07 22:43:37 +0530

    hi

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ console.log(url);
 MongoClient.connect(url, (err, client) => {
   if (err) throw err;
   app.locals.db = client.db("Deliciously");
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
     console.log(`sever running on ${PORT}`);
   });
@@ -28,7 +28,6 @@ app.use("/", require("./landing"));
 app.use("/signup", require("./signUp"));
 app.use("/login", require("./login"));
 app.use("/reset", require("./reset"));
-
 app.use("/recipe", require("./viewRecipe"));
 app.use("/recipes", require("./allRecipes"));
 app.use("/collection", require("./myCollection"));
--- myCollection.js
+++ myCollection.js
@@ -1,10 +1,44 @@
 const express = require("express");
 const router = express.Router();
 
+var recipeId;
+
+
 router.get("/", (req, res) => {
-  res.render("myCollection", {
-    style: "myCollection"
-  });
+  
+  var db = req.app.locals.db;
+ db.collection("myCollection")
+    .find({user:"Neyanta"})
+      .toArray((err, result) => {
+      if (err) return console.log(err);
+
+      console.log(result);
+      console.log(recipeId);
+      res.render("myCollection", 
+      { collection: result, style: "myCollection" });
+    });
 });
 
+router.post("/", (req, res) => {
+  var db = req.app.locals.db;
+  const newCollection = {
+    user: req.body.user,
+    recId: req.body.recipeId,
+    title: req.body.title
+  };
+  recipeId = req.body.recipeId;
+  console.log(newCollection);
+
+  db.collection("myCollection").insert(newCollection, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.redirect("collection");
+  });
+});
+   router.delete('/:id',function(req,res){
+  var db = req.app.locals.db;
+  db.collection('myCollection').deleteOne({recId: recipeId},function(err,result){
+    res.redirect("collection");
+  });
+});
 module.exports = router;
